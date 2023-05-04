0284ff5e855a3c076806bc5de92bc9fac68796b3
Svetlan Nikolov <104004467+sensgithub@users.noreply.github.com>
2023-05-04 18:28:08 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,3 +6,12 @@
     }
 
 ?>
+#
+<?php
+# for Local 
+#   $database= new mysqli("localhost","root","","ehospital");
+#   if ($database->connect_error){
+#        die("Connection failed:  ".$database->connect_error);
+#    }
+#
+?>
